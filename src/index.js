29c73e2b3744b61ce921f4199d916a01056e2f96
Layton Whiteley <layton.w@smart-host.com>
2020-10-02 22:18:11 +0200

    chore: add dist action

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ async function run() {
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const payload = JSON.stringify(github.context, undefined, 2);
     console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
