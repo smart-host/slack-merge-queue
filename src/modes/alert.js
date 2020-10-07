a5d53a870931f75662649009f084b1a06e7bc85a
Layton Whiteley <layton.w@smart-host.com>
2020-10-07 02:39:49 +0200

    0.0.14

--- src/modes/alert.js
+++ src/modes/alert.js
@@ -11,17 +11,18 @@ const {
 const { STATUS, Q_STATUS } = require('../consts');
 
 async function alert({ client, payload: orgPayload }) {
-  let issueNumber = get(orgPayload, 'pull_request.number');
+  const issueNumbers = [get(orgPayload, 'pull_request.number')];
   const onlyWhenCurrent =
     core.getInput('only_when_current').toLowerCase() === 'true';
 
   if (get(orgPayload, 'workflow_run')) {
-    issueNumber = get(orgPayload, 'workflow_run.pull_requests[0].number');
+    get(orgPayload, 'workflow_run.pull_requests', []).forEach(({ number }) => {
+      if (issueNumbers.includes(number)) {
+        return null;
+      }
+      issueNumbers.push(number);
+    });
   }
-  const payload = {
-    ...orgPayload,
-    issueNumber,
-  };
   const { messages = [] } = await getHistory({
     channel: core.getInput('channel'),
     client,
@@ -34,40 +35,47 @@ async function alert({ client, payload: orgPayload }) {
 
   const highestPriorityIndex = matches.length - 1;
   core.info(`Queue size: ${matches.length}`);
-  core.info(`Highest priority index: ${highestPriorityIndex} `);
+  core.info(`Highest priority index: ${highestPriorityIndex} \n`);
 
-  const match = findLast(matches, (message, i) => {
-    const { text } = message;
-    const { issueNumber: num } = parseTag(text);
-    const isCurrentPR = num.trim() === issueNumber.toString();
-    const shouldAlert =
-      !onlyWhenCurrent || (onlyWhenCurrent && i === highestPriorityIndex);
-    if (isCurrentPR) {
-      core.info(`current index: ${i}`);
+  const promises = issueNumbers.filter(Boolean).map(async (issueNumber) => {
+    const prTag = `PR${issueNumber}: `;
+    const match = findLast(matches, (message, i) => {
+      const { text } = message;
+      const { issueNumber: num } = parseTag(text);
+      const isCurrentPR = num.trim() === (issueNumber || '').toString();
+      const shouldAlert =
+        !onlyWhenCurrent || (onlyWhenCurrent && i === highestPriorityIndex);
+      if (isCurrentPR) {
+        core.info(`${prTag}index is ${i}`);
+      }
+      return shouldAlert && isCurrentPR;
+    });
+
+    if (!match) {
+      core.info(
+        `${prTag}Could not find pull request in queue or did not satisfy filters`,
+      );
+      return setActionStatus(STATUS.NOT_FOUND);
     }
-    return shouldAlert && isCurrentPR;
+
+    core.info(`${prTag}found PR in queue, sending alert in thread`);
+    core.debug(`${JSON.stringify(match, null, 2)}`);
+    let watchers = getWatchers(match);
+
+    const alertMsg = core.getInput('alert_message');
+    const alertText = `${alertMsg}${watchers}`;
+
+    await client.chat.postMessage({
+      thread_ts: match.ts,
+      mrkdwn: true,
+      text: alertText,
+      channel: core.getInput('channel'),
+      icon_emoji: core.getInput('icon_emoji'),
+      icon_url: core.getInput('icon_url'),
+    });
   });
 
-  if (!match) {
-    core.info(`Could not find pull request in queue or did not satify filters`);
-    return setActionStatus(STATUS.NOT_FOUND);
-  }
-
-  core.info(`found PR in queue, sending alert in thread`);
-  core.debug(`${JSON.stringify(match, null, 2)}`);
-  let watchers = getWatchers(match);
-
-  const alertMsg = core.getInput('alert_message');
-  const alertText = `${alertMsg}${watchers}`;
-
-  await client.chat.postMessage({
-    thread_ts: match.ts,
-    mrkdwn: true,
-    text: alertText,
-    channel: core.getInput('channel'),
-    icon_emoji: core.getInput('icon_emoji'),
-    icon_url: core.getInput('icon_url'),
-  });
+  await Promise.all(promises);
 
   return setActionStatus(STATUS.COMPLETED);
 }
