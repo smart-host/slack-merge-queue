61617eef40e29153a3074006ccf635bf59e6a0b7
Layton Whiteley <layton.w@smart-host.com>
2024-03-23 12:02:02 +0100

    chore: upgrade to run on node20

---
+++ node_modules/@slack/types/dist/block-kit/block-elements.js
@@ -0,0 +1,4 @@
+"use strict";
+// This file contains objects documented here: https://api.slack.com/reference/block-kit/block-elements
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=block-elements.js.map
---
+++ node_modules/@slack/types/dist/calls.js
@@ -0,0 +1,6 @@
+"use strict";
+// These types represent users in Slack Calls, which is an API for showing 3rd party calls within the Slack client.
+// More information on the API guide for Calls: https://api.slack.com/apis/calls
+// and on User objects for use with Calls: https://api.slack.com/apis/calls#users
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=calls.js.map
